283ca9592c868e44238b7e563954740fc4e3d298
krvchky <kravchukyana05@gmail.com>
2024-08-07 21:16:09 +0200

    update ImageCard

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ImageCard = ({ photo, onClick }) => {
   return (
-    <li className="image-card" onClick={() => onClick(photo)}>
+    <li className="image-card" >
       <div className="img-cover">
-        <img src={photo.urls.small} alt={photo.alt_description} className="card-img" />
+        <img src={photo.urls.small} alt={photo.alt_description} onClick={() => onClick(photo)} className="card-img" />
       </div>
     </li>
   );
